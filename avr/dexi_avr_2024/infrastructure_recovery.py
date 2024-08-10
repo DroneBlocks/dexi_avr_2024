18f9d2c400c589e33d90a225f2f927cf097397f2
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-08-10 17:14:27 -0500

    WIP

--- avr/dexi_avr_2024/infrastructure_recovery.py
+++ avr/dexi_avr_2024/infrastructure_recovery.py
@@ -1,20 +1,33 @@
-
+import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 
+# /avr/fire_laser
+# /avr/detect_beam
+
+    # Listen for tag detections to determine where failures are and light up LED white
+    # Look for IR signal to determine which color connex AVR drone should pick up
+    # Light up LED based on tag id
+    # Listen for IR signal
+    # Light up LED based on IR signal
+    # Fire laser
+
 class InfrastructureRecovery(Node):
     def __init__(self):
+        super().__init__('avr_node')
         self.get_logger().info("InfrastructureRecovery Node is running")
 
+    
+    
 def main(args=None):
     rclpy.init(args=args)
     try:
-        node = InfrastructureRecovery()
-        rclpy.spin(node)
+        avr_node = InfrastructureRecovery()
+        rclpy.spin(avr_node)
     except KeyboardInterrupt:
         pass
     except ExternalShutdownException:
-        node.destroy_node()
+        avr_node.destroy_node()
         rclpy.shutdown()
         sys.exit(1)
 
