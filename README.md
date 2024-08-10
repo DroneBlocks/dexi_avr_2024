ebc166dba53921ae8efa513276ab02010345bd89
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-08-10 15:35:22 -0500

    Setup

---
+++ README.md
@@ -0,0 +1,5 @@
+This repo requires the DEXI core ROS2 components. Clone this into the ~/dexi_ws/src folder and build:
+
+colcon build --packages-select apriltag_msgs
+colcon build --packages-select apriltag_ros
+colcon build --packages-select dexi_avr_2024
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'dexi_avr_2024'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='ubuntu@todo.todo',
-    description='TODO: Package description',
+    maintainer='DroneBlocks',
+    maintainer_email='db@droneblocks.io',
+    description='ROS2 for AVR 2024 Drone Competition',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
